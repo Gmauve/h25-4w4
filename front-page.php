8842d55f1751858168d535bba6a2895ab2127631
Guillaume Nagy <e2366802@cmaisonneuve.qc.ca>
2025-03-05 16:18:08 -0500

    s7c1 Début des custom fields

--- front-page.php
+++ front-page.php
@@ -1,12 +1,12 @@
     <?php get_header(); ?>
     <section class="hero">
         <div class="hero__contenu global">
-            <h1 class="hero__titre">Club de voyage</h1>
+            <h1 class="hero__titre"><?php bloginfo('name'); ?></h1>
             <p class="hero__description">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. In nulla quam ut necessitatibus nesciunt placeat provident id minus voluptate! Tenetur ea ad quidem a velit illum at aliquam aliquid ut.
+                <?php bloginfo('description'); ?>
             </p>
             <p class="hero__courriel">
-                <a href="#">info@cmaisonneuve.qc.ca</a>
+                <?php bloginfo('admin_email'); ?>
             </p>
             <p class="hero__adresse">
                 5800 Sherbrooke-est - Montréal (Québec) H1X 2A2
@@ -17,26 +17,6 @@
                 <img src="https://s2.svgbox.net/social.svg?ic=stackoverflow&color=000000" width="20" height="20">
                 <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
             </div>
-        </div>
-
-    </section>
-    <section class="galerie">
-        <div class="galerie global">
-            <figure class="galerie__figure">
-                <img src="" alt="" class="galerie__img">
-            </figure>
-            <figure class="galerie__figure">
-                <img src="" alt="" class="galerie__img">
-            </figure>
-            <figure class="galerie__figure">
-                <img src="" alt="" class="galerie__img">
-            </figure>
-            <figure class="galerie__figure">
-                <img src="" alt="" class="galerie__img">
-            </figure>
-            <figure class="galerie__figure">
-                <img src="" alt="" class="galerie__img">
-            </figure>
         </div>
     </section>
     <section class="populaire">
