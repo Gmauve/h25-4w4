a84dea5665ddb14681e872c5b89334d02fa6c219
Guillaume Nagy <e2366802@cmaisonneuve.qc.ca>
2025-03-25 15:41:38 -0400

    Création de la page 404

---
+++ 404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+    <section class="err">
+        <h1 class="err__titre"></h1>
+        <img src="" alt="" class="err_img">
+        <p class="err__description"></p>
+    </section>
+    <?php get_footer(); ?>
+</body>
+</html>
