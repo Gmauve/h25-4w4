02739846d031e13a4894f5221113d9d843b77b38
Guillaume Nagy <e2366802@cmaisonneuve.qc.ca>
2025-03-25 14:25:26 -0400

    readme mis à jour

---
+++ gabarits/hero.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Template-part héro
+ */
+?>
+<?php
+$hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
+$hero_background = get_theme_mod('hero_background', '');
+?>
+<section class="hero" style="background-image: url(<?= $hero_background ?>)">
+    <div class="hero__contenu global">
+        <h1 class="hero__titre"><?php bloginfo('name'); ?></h1>
+        <p class="hero__description">
+            <?php bloginfo('description'); ?>
+        </p>
+        <p class="hero__courriel">
+            <?php bloginfo('admin_email'); ?>
+        </p>
+        <p class="hero__adresse">
+            5800 Sherbrooke-est - Montréal (Québec) H1X 2A2
+        </p>
+        <p class="hero_auteur">Auteur : <?= $hero_auteur ?></p>
+        <div class="hero__icone">
+            <?php get_template_part( 'gabarits/social' ); ?>
+        </div>
+    </div>
+</section>
--- single-post.php
+++ single-post.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
-    <h1>-------- single-post.php ----------</h1>
     <section class="populaire">
         <div class="global">
             <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
@@ -11,6 +10,9 @@
                 ?>
                 <h2><?php the_title(); ?></h2>
                 <div><?php the_content() ?>
+                <p>Température maximum: <?php the_field('temperature_maximum') ?>&#176;C</p>
+                <p>Température moyenne: <?php the_field('temperature_moyenne') ?>&#176;C</p>
+                <p>Température minimum: <?php the_field('temperature_minimum') ?>&#176;C</p>
             <?php endwhile; endif; ?>
         </div>
     </section>
