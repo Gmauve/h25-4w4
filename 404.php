c8f5d1cbb687c81be4f82ddd6a703e8809763f0c
Guillaume Nagy <e2366802@cmaisonneuve.qc.ca>
2025-03-31 13:54:29 -0400

    Image de la page d'erreur 404 placée en arrière-plan

--- 404.php
+++ 404.php
@@ -1,12 +1,11 @@
 <?php
-    $description_404 = get_theme_mod('description_404', 'détails erreur 404');
+    $description_404 = get_theme_mod('description_404', 'message erreur 404');
     $image_404 = get_theme_mod('image_404', 'image erreur 404');
-    $titre_404 = get_theme_mod('titre_404', 'message erreur 404');
+    $titre_404 = get_theme_mod('titre_404', 'titre erreur 404');
 ?>
 <?php get_header(); ?>
-    <section class="err">
+    <section class="err" style="background-image: url(<?= $image_404 ?>);">
         <h1 class="err__titre"><?= $titre_404 ?></h1>
-        <img src="<?= $image_404 ?>" alt="" class="err_img">
         <p class="err__description"><?= $description_404 ?></p>
         <?php get_template_part( 'gabarits/social' ); ?>
     </section>
