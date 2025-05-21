ea59c9865289e6fcd8d232471882e7c099ca57f9
Guillaume Nagy <e2366802@cmaisonneuve.qc.ca>
2025-05-21 00:49:51 -0400

    Filtre de catégories à exclure dans les cartes de 'front-page' complété

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -16,6 +16,19 @@
     <h2 class="carte__titre"><?php the_title(); ?></h2>
     <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 10, " ... " ); ?></p>
     <a class="carte__bouton carte__bouton--actif" href="<?php the_permalink() ?>">suite ...</a>
-    <?php  the_category();  ?>
+    <?php  
+      $post_categories = get_the_category();
+      $cat_a_retirer = array(15); // Categories a exclure
+
+      foreach ($post_categories as $category) {
+        $filtered_categories = array_filter($post_categories, function($post_categories) use ($cat_a_retirer) {
+            return !in_array($post_categories->term_id, $cat_a_retirer);
+        });
+      }
+
+      foreach ($filtered_categories as $category) {
+          echo '<a href="' . esc_url(get_category_link($category->term_id)) . '">' . esc_html($category->name) . '</a> ';
+      }
+    ?>
   </div>
 </article>
